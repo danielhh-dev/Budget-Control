65d990475542b71a62dc39c5b6f3a076a58064b7
Daniel Hernandez <danielhh.dev@gmail.com>
2022-09-29 21:17:45 -0500

    Deploy file updated

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-# if you are deploying to https://danielhh-dev.github.io/<REPO>
+# if you are deploying to https://danielhh-dev.github.io/Budget-Control
 # git push -f git@github.com:danielhh-dev/Budget-Control.git main:gh-pages
 
 cd -
